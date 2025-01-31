dd42818aa52dcb21ab8f7c690b458290f7a71cb4
Jacek Szulc <j.szulc77@wp.pl>
2025-01-31 19:32:35 +0100

    v.11

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <div>
     <footer class="py-3 my-4">
-        <p class="text-center text-body-secondary"><span class="footer_year"></span> JS CKU_Sopot</p>
+        <p class="text-center text-body-secondary">
+            <span class="footer_year"></span> JS CKU_Sopot
+            <a href="https://github.com/JacekSzulc23143/ksiazkaAdresowa" target="_blank">
+                <i class="fa-brands fa-github"></i>
+            </a>
+        </p>
     </footer>
 </div>
