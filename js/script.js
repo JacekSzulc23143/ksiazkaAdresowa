034c51f6e99d0bbb4729567b65d0e177ca86caae
Jacek Szulc <j.szulc77@wp.pl>
2025-01-31 20:58:39 +0100

    v.12

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-const footerYear = document.querySelector(".footer_year");
 // rok w footer
+const footerYear = document.querySelector(".footer_year");
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 handleCurrentYear();
+
+// dark mode
+const button = document.getElementById("toggle-theme");
+button.addEventListener("click", () => {
+	document.body.classList.toggle("dark");
+});
--- nav.php
+++ nav.php
@@ -5,7 +5,7 @@ $titleNav = "książka<span>adresowa</span>";
 ?>
 
 <div class="title-text">
-    <h1><?php echo $titleNav; ?></h1>
+    <h1><i class="fa-solid fa-book"></i> <?php echo $titleNav; ?></h1>
 </div>
 
 <header>
@@ -14,6 +14,7 @@ $titleNav = "książka<span>adresowa</span>";
             <a href="index.php#"><i class="fa-solid fa-igloo"></i> Strona główna</a></li>
             <a href="add.php#"><i class="fa-solid fa-circle-plus"></i> Dodaj kontakt</a></li>
             <a href="about.php#"><i class="fa-solid fa-address-card"></i> O mnie</a></li>
+            <a id="toggle-theme"><i class="fa-solid fa-circle-half-stroke"></i></a>
         </div>
     </nav>
 </header>
